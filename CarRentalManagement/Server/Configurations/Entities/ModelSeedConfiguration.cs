b5a37aa743cf89516647d2b012738a18e1c6bf39
TONG GIA KHANH 2203548I <2203548I@student.tp.edu.sg>
2024-01-05 16:25:37 +0800

    Added REST API Endpoints and Unit Of Work pattern.

---
+++ CarRentalManagement/Server/Configurations/Entities/ModelSeedConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using CarRentalManagement.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarRentalManagement.Server.Configurations.Entities
+{
+    public class ModelSeedConfiguration : IEntityTypeConfiguration<Model>
+    {
+        public void Configure(EntityTypeBuilder<Model> builder)
+        {
+            builder.HasData(
+                new Model
+                {
+                    Id = 1,
+                    Name = "3 Series",
+                    CreatedDate = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Model
+                {
+                    Id = 2,
+                    Name = "X5",
+                    CreatedDate = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Model
+                {
+                    Id = 3,
+                    Name = "Prius",
+                    CreatedDate = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Model
+                {
+                    Id = 4,
+                    Name = "Rav4",
+                    CreatedDate = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                }
+                );
+        }
+    }
+}
